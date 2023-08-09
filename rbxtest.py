4d1512aef3b0501e727fba418b1f909869b2261f
Ar906 <141795484+Ar906@users.noreply.github.com>
2023-08-08 22:36:51 -0400

    Add files via upload

---
+++ rbxtest.py
@@ -0,0 +1,12 @@
+
+import requests
+
+place_id = 0  # User ID
+user_req = requests.get(f"https://apis.roblox.com/universes/v1/places/{place_id}/universe")
+user_data = user_req.json()
+
+uni_id = user_data["universeId"]
+game_check = requests.get(f"https://games.roblox.com/v1/games?universeIds={uni_id}")
+game_data = game_check.json()
+
+print(game_data["data"][0]["visits"])
